3dbbf8dc1b99d732871d0530d14595c03e4c9e09
dhana <ddhanarr@gmail.com>
2022-08-26 13:47:50 +0700

    feat: redeem ticket functionality

--- src/components/BarcodeScanner.tsx
+++ src/components/BarcodeScanner.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import BarcodeScannerComponent from "react-qr-barcode-scanner";
 import { Modal, ModalPropsInterface } from "./Modal";
+import toast, { Toaster } from "react-hot-toast";
+import Spinner from "../assets/Spinner";
 
 interface IBarcodeScanner
   extends Omit<ModalPropsInterface, "className" | "as" | "children"> {
@@ -12,18 +14,86 @@ export default function BarcodeScanner({
   setIsOpen,
   setBarcodeId,
 }: IBarcodeScanner) {
-  const [data, setData] = React.useState("Not Found");
+  const [ticketId, setTicketId] = React.useState<string>("");
+  const [isLoading, setIsLoading] = React.useState<boolean>(false);
+  const [ticketType, setTicketType] = React.useState<string>("ps1");
+  const [ticketResponse, setTicketResponse] = React.useState<any>();
+
+  const baseUrl =
+    ticketType === "ps1"
+      ? "https://admin.tesdeveloper.me/api/scan-tiket/2"
+      : "https://admin.tesdeveloper.me/api/scan-tiket";
+
+  const redeemHandler = async () => {
+    setIsLoading(true);
+    const ticket = await fetch(baseUrl, {
+      method: "POST",
+      body: JSON.stringify({ uniqueId: ticketId }),
+    });
+    if (!ticket.ok) {
+      toast.error("Error");
+    }
+    const response = await ticket.json();
+    if (response.message !== "Ticket successfully redeemed") {
+      toast.error(response.message);
+      setIsLoading(false);
+      return;
+    }
+    toast.success(response.message);
+    setIsLoading(false);
+    setIsOpen(false);
+  };
+
   return (
-    <Modal isOpen={isOpen} setIsOpen={setIsOpen}>
-      <BarcodeScannerComponent
-        width={500}
-        height={500}
-        onUpdate={(err, result) => {
-          if (result) setData(result.getText());
-          else setData("Not Found");
-        }}
-      />
-      <p>{data}</p>
-    </Modal>
+    <>
+      <Toaster />
+      <Modal isOpen={isOpen} setIsOpen={setIsOpen}>
+        <h3 className="text-center">Scan Ticket</h3>
+        {!ticketId ? (
+          <>
+            <div className="flex items-center mx-auto py-5 justify-center">
+              <label htmlFor="ticket-type">Tipe Tiket:</label>
+              <select
+                name="ticket-type"
+                id="ticket-type"
+                className="p-2 mx-2 rounded-md focus:outline-none focus:ring focus:ring-lime-200 w-[80%]"
+                onChange={(e) => setTicketType(e.target.value)}
+              >
+                <option value="ps1">Presale 1</option>
+                <option value="not-ps1">Selain Presale 1</option>
+              </select>
+            </div>
+            <BarcodeScannerComponent
+              height={500}
+              onUpdate={(err, result) => {
+                if (result) setTicketId(result.getText());
+              }}
+            />
+          </>
+        ) : (
+          <div>
+            <p className="py-5">
+              Ticket ID: <span className="font-semibold">{ticketId}</span>
+            </p>
+            <div className="flex justify-end gap-x-3">
+              <button
+                className="btn bg-red-600 text-white"
+                onClick={() => setTicketId("")}
+                disabled={isLoading}
+              >
+                Scan Again
+              </button>
+              <button
+                className="btn bg-green-600 text-white"
+                onClick={() => redeemHandler()}
+                disabled={isLoading}
+              >
+                {isLoading ? <Spinner /> : <p>Redeem</p>}
+              </button>
+            </div>
+          </div>
+        )}
+      </Modal>
+    </>
   );
 }
